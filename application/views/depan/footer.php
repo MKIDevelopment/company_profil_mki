216719eae27a1ae4c5cb739173af4db2d78ad95e
brada1604 <bagusnugroho104@gmail.com>
2022-05-31 11:23:43 +0700

    Update footer.php

--- application/views/depan/footer.php
+++ application/views/depan/footer.php
@@ -80,7 +80,7 @@
         var options = {
             whatsapp: "'<?php echo $title['identitas_nohp'];?> '", // WhatsApp number
             call_to_action: "Kirimi kami pesan", // Call to action
-            position: "right", // Position may be 'right' or 'left'
+            position: "left", // Position may be 'right' or 'left'
         };
         var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
