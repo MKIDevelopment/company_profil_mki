268717b08535b9b6cd84b376c9a239ce99051db0
brada1604 <bagusnugroho104@gmail.com>
2022-07-08 11:21:41 +0700

    clean code controller home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -19,29 +19,31 @@ class Home extends CI_Controller{
 	}
 
 	function index(){
-		$x['berita']=$this->M_tulisan->get_berita_home();
-		$x['pengumuman']=$this->M_pengumuman->get_pengumuman_home();
-		$x['contact']=$this->M_identitas->get_all_pengguna();
-		$x['agenda']=$this->M_agenda->get_agenda_home();
-		$x['tot_files']=$this->db->get('tbl_files')->num_rows();
-		$x['tot_agenda']=$this->db->get('tbl_agenda')->num_rows();
-		$x['faq_l']=$this->db->query("SELECT * FROM tbl_faq WHERE status='1' ORDER BY status");
-		$x['faq_r']=$this->db->query("SELECT * FROM tbl_faq WHERE status='2' ORDER BY status");
+		$x['agenda'] = $this->M_agenda->get_agenda_home();
+		$x['alb'] = $this->M_album->get_all_album();
 		$x['baner'] = $this->M_baner->get_all_baner();
-		$x['limit']=$this->M_tulisan->limit_berita();
-		$x['testi']=$this->M_testimoni->get_all_testimoni();
-		$x['data']=$this->M_identitas->get_all_pengguna();
-		$x['gal']=$this->M_galeri->get_all_galeri();
-		$x['alb']=$this->M_album->get_all_album();
-		$x['point']=$this->M_point->get_all_point();
+		$x['berita'] = $this->M_tulisan->get_berita_home();
+		$x['contact'] = $this->M_identitas->get_all_pengguna();
+		$x['data'] = $this->M_identitas->get_all_pengguna();
+		$x['gal'] = $this->M_galeri->get_all_galeri();
+		$x['limit'] = $this->M_tulisan->limit_berita();
+		$x['pengumuman'] = $this->M_pengumuman->get_pengumuman_home();
+		$x['testi'] = $this->M_testimoni->get_all_testimoni();
+		$x['tot_files'] = $this->db->get('tbl_files')->num_rows();
+		$x['tot_agenda'] = $this->db->get('tbl_agenda')->num_rows();
+		$x['point'] = $this->M_point->get_all_point();
+		
+		$x['faq_l'] = $this->db->query("SELECT * FROM tbl_faq WHERE status='1' ORDER BY status");
+		$x['faq_r'] = $this->db->query("SELECT * FROM tbl_faq WHERE status='2' ORDER BY status");
+		
 		$this->load->view('depan/index',$x);
 	}
 
 	function kirim_pesan(){
-		$nama=htmlspecialchars($this->input->post('xnama',TRUE),ENT_QUOTES);
-		$email=htmlspecialchars($this->input->post('xemail',TRUE),ENT_QUOTES);
-		$kontak=htmlspecialchars($this->input->post('xphone',TRUE),ENT_QUOTES);
-		$pesan=htmlspecialchars($this->input->post('xmessage',TRUE),ENT_QUOTES);
+		$nama = htmlspecialchars($this->input->post('xnama',TRUE),ENT_QUOTES);
+		$email = htmlspecialchars($this->input->post('xemail',TRUE),ENT_QUOTES);
+		$kontak = htmlspecialchars($this->input->post('xphone',TRUE),ENT_QUOTES);
+		$pesan = htmlspecialchars($this->input->post('xmessage',TRUE),ENT_QUOTES);
 		$this->M_kontak->kirim_pesan($nama,$email,$kontak,$pesan);
 		echo $this->session->set_flashdata('msg','success');
 		redirect('home');
